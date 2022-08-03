4de5359362b1d02b467b983764ab517e6785f2cb
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-03 21:16:58 +0200

    feat: integrate Prisma client with mocking

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,32 @@
 import express from "express";
 import "express-async-errors";
 
+import prisma from "./lib/prisma/client";
+
 const app = express();
 
-app.get("/planets", (request, response) => {
-    response.json([{ name: "Mercury" }, { name: "Venus" }]);
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany();
+
+    response.json(planets);
 });
 
 export default app;
+
+// // GET /planets - Retrieve all planets
+// app.get("/planets", (request, response) => {});
+
+// // GET /planets/:id - Retrieve a specific planet
+// app.get("/planets/:id", (request, response) => {});
+
+// // POST /planets - Create a new planet
+// app.post("/planets", (request, response) => {});
+
+// // PUT /planets/:id - Replace an existing planet
+// app.put("/planets/:id", (request, response) => {});
+
+// // DELETE /planets/:id - Delete a planet
+// app.delete("/planets/:id", (request, response) => {});
+
+// // POST /planets/:id/photo - Add a photo for the planet
+// app.post("/planets/:id/photo", (request, response) => {});
--- src/server.test.ts
+++ src/server.test.ts
@@ -1,13 +1,49 @@
 import supertest from "supertest";
+
+import { prismaMock } from "./lib/prisma/client.mock";
+
 import app from "./app";
 
 const request = supertest(app);
 
 test("GET /planets", async () => {
+    const planets = [
+        {
+            id: 2,
+            name: "Venus",
+            description: null,
+            diameter: 2232,
+            moons: 2,
+            createdAt: "2022-08-03T08:59:56.849Z",
+            updatedAt: "2022-08-03T08:59:45.438Z",
+        },
+        {
+            id: 1,
+            name: "Mercury",
+            description: null,
+            diameter: 1234,
+            moons: 3,
+            createdAt: "2022-08-03T08:59:29.566Z",
+            updatedAt: "2022-08-03T08:59:56.850Z",
+        },
+        {
+            id: 3,
+            name: "Mars",
+            description: null,
+            diameter: 232,
+            moons: 2,
+            createdAt: "2022-08-03T18:57:53.810Z",
+            updatedAt: "2022-08-03T18:57:42.947Z",
+        },
+    ];
+
+    // @ts-ignore
+    prismaMock.planet.findMany.mockResolvedValue(planets);
+
     const response = await request
         .get("/planets")
         .expect(200)
         .expect("Content-Type", /application\/json/);
 
-    expect(response.body).toEqual([{ name: "Mercury" }, { name: "Venus" }]);
+    expect(response.body).toEqual(planets);
 });
