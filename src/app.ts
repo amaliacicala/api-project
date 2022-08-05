46af337a1ace3bb22a24a1cdb7c6a8e0a5b03480
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-05 19:10:56 +0200

    feat: clear app.ts from comments

--- src/app.ts
+++ src/app.ts
@@ -93,21 +93,3 @@ app.delete("/planets/:id(\\d+)", async (request, response, next) => {
 app.use(validationErrorMiddleware);
 
 export default app;
-
-// // GET /planets - Retrieve all planets
-// app.get("/planets", (request, response) => {});
-
-// // GET /planets/:id - Retrieve a specific planet
-// app.get("/planets/:id", (request, response) => {});
-
-// // POST /planets - Create a new planet
-// app.post("/planets", (request, response) => {});
-
-// // PUT /planets/:id - Replace an existing planet
-// app.put("/planets/:id", (request, response) => {});
-
-// // DELETE /planets/:id - Delete a planet
-// app.delete("/planets/:id", (request, response) => {});
-
-// // POST /planets/:id/photo - Add a photo for the planet
-// app.post("/planets/:id/photo", (request, response) => {});
