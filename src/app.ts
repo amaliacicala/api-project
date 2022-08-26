7443b945c99d49316eedb0c2789e5c78c1f1504d
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-26 15:52:38 +0200

    feat: refactor planets routes out of main app script

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
-
-import prisma from "./lib/prisma/client";
-
-import {
-    validate,
-    validationErrorMiddleware,
-    planetSchema,
-    PlanetData,
-} from "./lib/validation";
-
-import { initMulterMiddleware } from "./lib/middleware/multer";
-
-const upload = initMulterMiddleware();
+import { validationErrorMiddleware } from "./lib/validation";
+import planetsRoutes from "./routes/planets";
 
 const corsOptions = {
     origin: "http://localhost:8080",
@@ -25,112 +14,7 @@ app.use(express.json());
 
 app.use(cors(corsOptions));
 
-// GET /planets - Retrieve all planets
-app.get("/planets", async (request, response) => {
-    const planets = await prisma.planet.findMany();
-
-    response.json(planets);
-});
-
-// GET /planets/:id - Retrieve a specific planet
-app.get("/planets/:id(\\d+)", async (request, response, next) => {
-    const planetId = Number(request.params.id);
-
-    const planet = await prisma.planet.findUnique({
-        where: { id: planetId },
-    });
-
-    if (!planet) {
-        response.status(404);
-        return next(`Cannot GET /planets/${planetId}`);
-    }
-
-    response.json(planet);
-});
-
-// POST /planets - Create a new planet
-app.post(
-    "/planets",
-    validate({ body: planetSchema }),
-    async (request, response) => {
-        const planetData: PlanetData = request.body;
-
-        const planet = await prisma.planet.create({
-            data: planetData,
-        });
-
-        response.status(201).json(planet);
-    }
-);
-
-// PUT /planets/:id - Replace an existing planet
-app.put(
-    "/planets/:id(\\d+)",
-    validate({ body: planetSchema }),
-    async (request, response, next) => {
-        const planetId = Number(request.params.id);
-        const planetData: PlanetData = request.body;
-
-        try {
-            const planet = await prisma.planet.update({
-                where: { id: planetId },
-                data: planetData,
-            });
-
-            response.status(200).json(planet);
-        } catch (error) {
-            response.status(404);
-            next(`Cannot PUT /planets/${planetId}`);
-        }
-    }
-);
-
-// DELETE /planets/:id - Delete a planet
-app.delete("/planets/:id(\\d+)", async (request, response, next) => {
-    const planetId = Number(request.params.id);
-
-    try {
-        await prisma.planet.delete({
-            where: { id: planetId },
-        });
-
-        response.status(204).end();
-    } catch (error) {
-        response.status(404);
-        next(`Cannot DELETE /planets/${planetId}`);
-    }
-});
-
-// POST /planets/:id/photo - Upload a photo to a planet
-app.post(
-    "/planets/:id(\\d+)/photo",
-    upload.single("photo"),
-    async (request, response, next) => {
-        // if there's no file
-        if (!request.file) {
-            response.status(400);
-            return next("No photo file uploaded.");
-        }
-
-        const planetId = Number(request.params.id);
-        const photoFilename = request.file.filename;
-
-        try {
-            await prisma.planet.update({
-                where: { id: planetId },
-                data: { photoFilename },
-            });
-
-            response.status(201).json({ photoFilename });
-        } catch (error) {
-            response.status(404);
-            next(`Cannot POST /planets/${planetId}/photo`);
-        }
-    }
-);
-
-// view uploaded photos in the browser - http://localhost:3000/planets/photos/{filename.extension}
-app.use("/planets/photos", express.static("uploads"));
+app.use("/planets", planetsRoutes);
 
 app.use(validationErrorMiddleware);
 
---
+++ src/routes/planets.ts
@@ -0,0 +1,124 @@
+import express, { Router } from "express";
+
+import prisma from "../lib/prisma/client";
+
+import {
+    validate,
+    planetSchema,
+    PlanetData,
+} from "../lib/validation";
+
+import { initMulterMiddleware } from "../lib/middleware/multer";
+
+const upload = initMulterMiddleware();
+
+const router = Router();
+
+// GET /planets - Retrieve all planets
+router.get("/", async (request, response) => {
+    const planets = await prisma.planet.findMany();
+
+    response.json(planets);
+});
+
+// GET /planets/:id - Retrieve a specific planet
+router.get("/:id(\\d+)", async (request, response, next) => {
+    const planetId = Number(request.params.id);
+
+    const planet = await prisma.planet.findUnique({
+        where: { id: planetId },
+    });
+
+    if (!planet) {
+        response.status(404);
+        return next(`Cannot GET /planets/${planetId}`);
+    }
+
+    response.json(planet);
+});
+
+// POST /planets - Create a new planet
+router.post(
+    "/",
+    validate({ body: planetSchema }),
+    async (request, response) => {
+        const planetData: PlanetData = request.body;
+
+        const planet = await prisma.planet.create({
+            data: planetData,
+        });
+
+        response.status(201).json(planet);
+    }
+);
+
+// PUT /planets/:id - Replace an existing planet
+router.put(
+    "/:id(\\d+)",
+    validate({ body: planetSchema }),
+    async (request, response, next) => {
+        const planetId = Number(request.params.id);
+        const planetData: PlanetData = request.body;
+
+        try {
+            const planet = await prisma.planet.update({
+                where: { id: planetId },
+                data: planetData,
+            });
+
+            response.status(200).json(planet);
+        } catch (error) {
+            response.status(404);
+            next(`Cannot PUT /planets/${planetId}`);
+        }
+    }
+);
+
+// DELETE /planets/:id - Delete a planet
+router.delete("/:id(\\d+)", async (request, response, next) => {
+    const planetId = Number(request.params.id);
+
+    try {
+        await prisma.planet.delete({
+            where: { id: planetId },
+        });
+
+        response.status(204).end();
+    } catch (error) {
+        response.status(404);
+        next(`Cannot DELETE /planets/${planetId}`);
+    }
+});
+
+// POST /planets/:id/photo - Upload a photo to a planet
+router.post(
+    "/:id(\\d+)/photo",
+    upload.single("photo"),
+    async (request, response, next) => {
+        // if there's no file
+        if (!request.file) {
+            response.status(400);
+            return next("No photo file uploaded.");
+        }
+
+        const planetId = Number(request.params.id);
+        const photoFilename = request.file.filename;
+
+        try {
+            await prisma.planet.update({
+                where: { id: planetId },
+                data: { photoFilename },
+            });
+
+            response.status(201).json({ photoFilename });
+        } catch (error) {
+            response.status(404);
+            next(`Cannot POST /planets/${planetId}/photo`);
+        }
+    }
+);
+
+// view uploaded photos in the browser - http://localhost:3000/planets/photos/{filename.extension}
+router.use("/photos", express.static("uploads"));
+
+export default router;
