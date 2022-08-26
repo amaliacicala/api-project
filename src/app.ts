80c54bc07651fd3892403fad9776aae0d04bad2e
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-26 15:57:31 +0200

    feat: reorganise middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
 import express from "express";
 import "express-async-errors";
-import cors from "cors";
-import { validationErrorMiddleware } from "./lib/validation";
+import { validationErrorMiddleware } from "./lib/middleware/validation";
 import planetsRoutes from "./routes/planets";
-
-const corsOptions = {
-    origin: "http://localhost:8080",
-};
+import { initCorsMiddleware } from "./lib/middleware/cors";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors(corsOptions));
+app.use(initCorsMiddleware());
 
 app.use("/planets", planetsRoutes);
 
