98c44b7db3ecd3f13177dd5a9ccce03261d21927
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-27 16:36:03 +0200

    feat: configure sessions for production

--- src/lib/middleware/session.ts
+++ src/lib/middleware/session.ts
@@ -1,10 +1,16 @@
 import config from "../../config";
 import session from "express-session";
 
-export function initSessionMiddleware() {
+export function initSessionMiddleware(appEnvironment: string) {
+    const isProduction = appEnvironment === "production";
+
     return session({
         secret: config.SESSION_SECRET, // what will be used to encrypt our session cookies
         resave: false,
         saveUninitialized: false,
+        cookie: {
+            secure: isProduction,
+        },
+        proxy: isProduction,
     });
 }
